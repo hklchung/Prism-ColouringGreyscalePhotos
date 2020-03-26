594a8fa0a54158a1727cc9da7617b5a6d5ad6855
Leslie Chung <hklchung@gmail.com>
2020-03-26 21:31:26 +1100

    Adding results

--- main.py
+++ main.py
@@ -99,33 +99,6 @@ model.fit(x=X,
     epochs=1000)
 
 #============Test the model==============
-#test = X[split:]
-#test = [1.0/225*x for x in test]
-#test[0].shape
-
-#output = model.predict(test[0][:,:,0].reshape(1, 400, 400, 1))
-#output *= 128
-# Output colorizations
-#cur = np.zeros((400, 400, 3))
-#cur[:,:,0] = test[0][:,:,0]
-#cur[:,:,1:] = output[0]
-#imsave("color.png", lab2rgb(cur))
-
-
-#test = Xtrain[101]
-#test = test.reshape(400,400)
-#test.shape
-#test *= 128
-#output = model.predict(test.reshape(1, 400, 400, 1))
-#output *= 128
-#output = output.reshape(400,400,2)
-#output.shape
-#cur = np.zeros((400, 400, 3))
-#cur[:,:,0] = test
-#cur[:,:,1:] = output
-#imsave("test1_bw.png", test)
-#imsave("test1_color.png", lab2rgb(cur))
-
 output = model.predict(X[0].reshape(1,400,400,1))
 output *= 128
 # Output colorizations
@@ -134,8 +107,50 @@ cur[:,:,0] = X[0][:,:,0]
 cur[:,:,1:] = output[0]
 imsave("img_result.png", lab2rgb(cur))
 imsave("img_gray_version.png", rgb2gray(lab2rgb(cur)))
+
 #============Save model=================
 model_json = model.to_json()
 with open("model.json", "w") as json_file:
     json_file.write(model_json)
 model.save_weights("model.h5")
+
+#===========Resize test image============
+for filename in os.listdir('Image/Test'):
+    temp = Image.open('Image/Test/' + filename)
+    size = 400, 400
+    temp.thumbnail(size, Image.ANTIALIAS)
+    temp.save('Image/Test/Resized/' + filename, "JPEG")
+    
+#===========Get test images=============
+test_images = []
+for filename in os.listdir('Image/Test/Resized'):
+    temp = np.array(img_to_array(load_img('Image/Test/Resized/'+filename)), dtype=float)
+    hor = 400 - temp.shape[0]
+    ver = 400 - temp.shape[1]
+    if hor%2 != 0:
+        temp = np.pad(temp, ((hor//2 + 1, hor//2), (ver//2, ver//2), (0, 0)),
+              mode='constant', constant_values=0)
+    elif ver%2 != 0:
+        temp = np.pad(temp, ((hor//2, hor//2), (ver//2 + 1, ver//2), (0, 0)),
+              mode='constant', constant_values=0)
+    else:
+    # pad resized images such that they are all 400x400x3
+        temp = np.pad(temp, ((hor//2, hor//2), (ver//2, ver//2), (0, 0)),
+              mode='constant', constant_values=0)
+    test_images.append(np.array(temp, dtype=float))
+    
+#===========Transform test images=======
+test_X = [rgb2lab(1.0/255*x)[:,:,0] for x in test_images]
+test_X = [x.reshape(400, 400, 1) for x in test_X]
+test_X = np.array(test_X)
+
+#===========Colourise===================
+for i in range(0,len(test_X)):
+    output = model.predict(test_X[i].reshape(1,400,400,1))
+    output *= 128
+    # Output colorizations
+    cur = np.zeros((400, 400, 3))
+    cur[:,:,0] = test_X[i][:,:,0]
+    cur[:,:,1:] = output[0]
+    imsave("test{}_img_result.png".format(i+1), lab2rgb(cur))
+    imsave("test{}_img_gray_version.png".format(i+1), rgb2gray(lab2rgb(cur)))
