95ca07ec09851eb170de4913330811b076fd07c0
Leslie Chung <hklchung@gmail.com>
2020-03-28 18:41:23 +1100

    First functional Prism release

--- Prism.py
+++ Prism.py
@@ -10,12 +10,14 @@ import os
 import pathlib
 from PIL import Image, ImageOps
 import random
+from datetime import datetime
 import tensorflow as tf
 from tensorflow.python.client import device_lib
 
 class Prism:
     def __init__(self):
         self.dimls = [128, 256, 400]
+        self.start = datetime.utcnow()
         print("Welcome to Prism!")
         print("Will you be training a model today? [y/n]")
         self.train = input()
@@ -55,7 +57,7 @@ class Prism:
                 print("Found 'Train' folder with {} files".format(self.train_size))
                 print("Found 'Test' folder with {} files".format(self.test_size))
             else:
-                print("[Error] Missing one or both Train/Test folders in path")
+                print("[Error] Missing one or both Train/Test folders in path.")
                 print("[Error] The required structure is:")
                 print("[Error]     Your project folder")
                 print("[Error]          L Images")
@@ -65,15 +67,40 @@ class Prism:
                 exit()
             
             # Resize images
-            print("Resizing images in the Train folder")
+            print("Resizing images in the Train folder...")
             self.resize_image(self.train_pwd)
-            print("Resizing images in the Test folder")
+            print("---Time taken since program started: {} minutes".format((datetime.utcnow() - self.start).seconds/60)) 
+            print("Resizing images in the Test folder...")
             self.resize_image(self.test_pwd)
+            print("---Time taken since program started: {} minutes".format((datetime.utcnow() - self.start).seconds/60))
             
             # Load train images
-            print("Loading the resized images for training")
+            print("Loading the resized images for training...")
             images = self.load_image(self.train_pwd)
-        
+            print("---Time taken since program started: {} minutes".format((datetime.utcnow() - self.start).seconds/60))
+            
+            # Transform train images
+            print("Transforming the images for training...")
+            X, Y = self.train_transform_image(images)
+            print("---Time taken since program started: {} minutes".format((datetime.utcnow() - self.start).seconds/60))
+            
+            # Initialise model
+            print("Initialising model.")
+            model = self.initialise_model()
+            
+            # Train model
+            print("Training model...")
+            print("Settings: batch size = 8, no. of epochs = {}".format(self.epochs))
+            model.fit(x=X, 
+                      y=Y,
+                      batch_size=8,
+                      epochs=self.epochs)
+            
+            # Save model
+            print("Saving model...")
+            self.save_model(model)
+            print("---Time taken since program started: {} minutes".format((datetime.utcnow() - self.start).seconds/60))
+            
         if self.train == 'n':
             # Training + Output dimensions
             print("Please select your desired output dimensions.")
@@ -86,6 +113,7 @@ class Prism:
                 self.dim = int(round(float(input())))
                 if self.dim in range(0, 3):
                     break
+            self.dims = self.dimls[self.dim]
             
             # Detect current path and see if train/test folders available
             self.pwd = str(pathlib.Path().absolute())
@@ -94,7 +122,7 @@ class Prism:
                 self.test_size = len([x for x in os.listdir(self.test_pwd) if os.path.isfile(os.path.join(self.test_pwd, x))])
                 print("Found 'Test' folder with {} files".format(self.test_size))
             else:
-                print("[Error] Missing Test folder in path")
+                print("[Error] Missing Test folder in path.")
                 print("[Error] The required structure is:")
                 print("[Error]     Your project folder")
                 print("[Error]          L Images")
@@ -104,12 +132,40 @@ class Prism:
                 exit()
             
             # Resize images
-            print("Resizing images in the Test folder")
+            print("Resizing images in the Test folder...")
             self.resize_image(self.test_pwd)
+            print("---Time taken since program started: {} minutes".format((datetime.utcnow() - self.start).seconds/60))
             
             # Load images
-            print("Loading the resized images")
+            print("Loading the resized images...")
             images = self.load_image(self.test_pwd)
+            print("---Time taken since program started: {} minutes".format((datetime.utcnow() - self.start).seconds/60))
+            
+            # Transform train images
+            print("Transforming the images for training...")
+            X = self.test_transform_image(images)
+            print("---Time taken since program started: {} minutes".format((datetime.utcnow() - self.start).seconds/60))
+            
+            # Load model
+            print("Loading model...")
+            print("Do you wish to use the Prism pre-trained model? [y/n]")
+            self.model_choice = input()
+            if self.model_choice == 'y':
+                model = self.initialise_model()
+                model.load_weights('model_v2.h5')
+            if self.model_choice == 'n':
+                modells = [x for x in os.listdir(self.pwd + '\\Model') if os.path.isfile(os.path.join(self.pwd + '\\Model', x)) & x.endswith(".h5")]
+                print("Please select the model you would like to use.")
+                for i in range(0, len(modells)):
+                    print("[{}: {}]".format(i, modells[i]))
+                self.user_model = int(round(float(input())))
+                model = self.initialise_model()
+                model.load_weights(self.pwd + '\\Model\\' + modells[self.user_model])
+            print("---Time taken since program started: {} minutes".format((datetime.utcnow() - self.start).seconds/60))
+            
+            # Colourise images
+            self.result_pwd = self.pwd + '\\Result'
+            self.colourise(model, X, self.result_pwd)
                 
     def resize_image(self, path):
         resize_folder = path + '\\Resized'
@@ -142,3 +198,57 @@ class Prism:
                               mode='constant', constant_values=0)
             images.append(np.array(temp, dtype=float))
         return(images)
+        
+    def train_transform_image(self, images):
+        X = [rgb2lab(1.0/255*x)[:,:,0] for x in images]
+        Y = [rgb2lab(1.0/255*x)[:,:,1:] for x in images]
+        Y = [x/128 for x in Y]
+        X = [x.reshape(self.dims, self.dims, 1) for x in X]
+        Y = [x.reshape(self.dims, self.dims, 2) for x in Y]
+        X = np.array(X)
+        Y = np.array(Y)
+        return(X, Y)
+        
+    def test_transform_image(self, images):
+        X = [rgb2lab(1.0/255*x)[:,:,0] for x in images]
+        X = [x.reshape(self.dims, self.dims, 1) for x in X]
+        X = np.array(X)
+        return(X)
+        
+    def initialise_model(self):
+        model = Sequential()
+        model.add(InputLayer(input_shape=(400, 400, 1)))
+        model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+        model.add(Conv2D(32, (3, 3), activation='relu', padding='same', strides=2))
+        model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+        model.add(Conv2D(64, (3, 3), activation='relu', padding='same', strides=2))
+        model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+        model.add(Conv2D(128, (3, 3), activation='relu', padding='same', strides=2))
+        model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+        model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+        model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+        model.add(UpSampling2D((2, 2)))
+        model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+        model.add(UpSampling2D((2, 2)))
+        model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
+        model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
+        model.add(UpSampling2D((2, 2)))
+        model.compile(optimizer='rmsprop', loss='mse')
+        return(model)
+    
+    def save_model(self, model):
+        model_pwd = self.pwd + '\\Model'
+        model_json = model.to_json()
+        with open(model_pwd + "\\model_{}.json".format(str(datetime.date(datetime.now()))), "w") as json_file:
+            json_file.write(model_json)
+        model.save_weights(model_pwd + "\\model_{}.h5".format(str(datetime.date(datetime.now()))))
+        
+    def colourise(self, model, X, pwd):
+        for i in range(0,len(X)):
+            output = model.predict(X[i].reshape(1,self.dims,self.dims,1))
+            output *= 128
+            # Output colorizations
+            cur = np.zeros((self.dims, self.dims, 3))
+            cur[:,:,0] = X[i][:,:,0]
+            cur[:,:,1:] = output[0]
+            imsave(pwd + "\\" + "test{}_img_result.png".format(i+1), lab2rgb(cur))
